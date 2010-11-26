1e06f2acc0638033aa28972723fe1f6aa61c4ef4
Nathan Freitas <nathan@freitas.net>
2010-11-26 11:27:55 -0500

    Initial checking, based on Thai Language Pack project from AnySoftKeyboard Google Code Project

---
+++ src/com/anysoftkeyboard/languagepack/tibetan/KeyboardBroadcastReceiver.java
@@ -0,0 +1,16 @@
+package com.anysoftkeyboard.languagepack.tibetan;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Typeface;
+
+public class KeyboardBroadcastReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context arg0, Intent arg1) {
+
+    
+    }
+
+}
